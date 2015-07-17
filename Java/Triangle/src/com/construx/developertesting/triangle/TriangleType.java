71527c0fdb8ebb269be8afa8a199a0eb1b245acd
Melvin Pérez-Cedano <merezazo@gmail.com>
2015-07-17 01:55:01 -0700

    Cleanup

--- Java/Triangle/src/com/construx/developertesting/triangle/TriangleType.java
+++ Java/Triangle/src/com/construx/developertesting/triangle/TriangleType.java
@@ -1,13 +1,37 @@
 package com.construx.developertesting.triangle;
 
 public enum TriangleType {
-	EQUILATERAL, INVALID, ISOSCELES, SCALENE;
+	INVALID, SCALENE, EQUILATERAL, ISOSCELES;
 
-	public static TriangleType classify(int a, int b, int c) {
-		if (a == b && b == c)
-			return EQUILATERAL;
-		else if (a == b || b == c || c == a)
-			return ISOSCELES;
-		return SCALENE;
+	public static TriangleType identify(final int a, final int b, final int c) {
+		if (sideLenghtsFormATriangle(a, b, c))
+			if (allSidelengthsAreEqual(a, b, c))
+				return EQUILATERAL;
+			else if (twoOfTheSidelengthsAreEqual(a, b, c))
+				return ISOSCELES;
+			else
+				return SCALENE;
+		else
+			return INVALID;
+	}
+
+	private static boolean twoOfTheSidelengthsAreEqual(int a, int b, int c) {
+		return a == b || b == c || c == a;
+	}
+
+	private static boolean allSidelengthsAreEqual(int a, int b, int c) {
+		return a == b && b == c;
+	}
+
+	/**
+	 * The sum of the lengths of any two sides must be greater than or equal to
+	 * the length of the remaining side. However, although valid, this
+	 * implementation considers the degenerate case (x + y = z) invalid.
+	 * 
+	 * @return True if the sum of the lengths of any two sides is greater than
+	 *         the length of the remaining side
+	 */
+	private static boolean sideLenghtsFormATriangle(int a, int b, int c) {
+		return a + b > c && b + c > a && a + c > b;
 	}
 }
